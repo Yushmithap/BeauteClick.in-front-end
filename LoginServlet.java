15d1f7135006fd05651fdd99798c7e517335a464
Yushmithap <yushmitha6a1a@gmail.com>
2016-12-06 21:34:26 +0530

    Add files via upload

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,11 +1,16 @@
 package com.niit.loginapp.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.niit.loginapp.dao.UserDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -37,16 +42,19 @@ public class LoginServlet extends HttpServlet {
 		String username = (String)  request.getParameter("username");
 		String password = (String)  request.getParameter("password");
 		
-		System.out.println("Username:"+username);
-		System.out.println("Password:"+password);
+		UserDAO userDAO = new UserDAO();
+		RequestDispatcher dispatcher;
 		
-		if(username.equals("niit") && password.equals("niit")){
-			System.out.println("Credentials are correct");
+		if(userDAO.isValidUser(username,password)){
+			dispatcher = request.getRequestDispatcher("Home.jsp");
+			dispatcher.forward(request, response);
 		}
 		else{
-			System.out.println("Credentials are incorrect");
-		}
+			dispatcher = request.getRequestDispatcher("Login.jsp");
+			PrintWriter writer = response.getWriter();
+			writer.println("Invalid Credentials.Try Again...");
+			dispatcher.include(request, response);
 		}
 	}
-
-
+}
+		
