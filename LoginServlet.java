b2abc3e83b9c4d76eecdaffa51b65ad528029552
Yushmithap <yushmitha6a1a@gmail.com>
2016-12-09 10:39:39 +0530

    This is my login servlet
    This is my login servlet

---
+++ LoginServlet.java
@@ -0,0 +1,61 @@
+package com.niit.loginapp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.niit.loginapp.dao.UserDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+   
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Calling the method get");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Calling the method post");
+		
+		String username = (String)  request.getParameter("username");
+		String password = (String)  request.getParameter("password");
+		response.setContentType("text/html");
+		
+		UserDAO userDAO = new UserDAO();
+		RequestDispatcher dispatcher;
+		
+		if(userDAO.isValidUser(username,password)){
+			dispatcher = request.getRequestDispatcher("Home.jsp");
+			dispatcher.forward(request, response);
+		}
+		else{
+			dispatcher = request.getRequestDispatcher("Login.jsp");
+			PrintWriter writer = response.getWriter();
+			writer.println("Invalid Credentials.Try Again...");
+			dispatcher.include(request, response);
+		}
+	}
+}
+		
