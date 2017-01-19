215a57fb4afd9bc37bee2c24aaba6623cc245cae
Yushmithap <yushmitha6a1a@gmail.com>
2017-01-19 07:35:58 +0530

    Front end

--- HomeController.java
+++ HomeController.java
@@ -1,75 +1,98 @@
 package com.niit.shopgirl.Controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.servlet.ModelAndView;
 
-import com.niit.shopgirlbackend.daoimpl.UserDAOImpl;
+import com.niit.shopgirlbackend.dao.CategoryDAO;
+import com.niit.shopgirlbackend.dao.SupplierDAO;
+import com.niit.shopgirlbackend.dao.UserDAO;
 
-
-
+import com.niit.shopgirlbackend.model.Category;
+import com.niit.shopgirlbackend.model.Product;
+import com.niit.shopgirlbackend.model.Supplier;
+import com.niit.shopgirlbackend.model.User;
 
 @Controller
 public class HomeController {
 
+	private static Logger log = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private UserDAO userDAO;
+
+	@Autowired
+	private User user;
+
+	@Autowired
+	private Category category;
+
+	@Autowired
+	private CategoryDAO categoryDAO;
+
+	@Autowired
+	private Supplier supplier;
+
+	@Autowired
+	private SupplierDAO supplierDAO;
+
+	@Autowired
+	private Product product;
+
 	@RequestMapping("/")
-	public ModelAndView homePage(){
-		ModelAndView mv = new ModelAndView("home");
-		mv.addObject("homePage","true");
+	public ModelAndView onLoad(HttpSession session) {
+		log.debug("Starting of the method onLoad");
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("homePage", "true");
+		session.setAttribute("category", category);
+		session.setAttribute("product", product);
+		session.setAttribute("supplier", supplier);
+
+		session.setAttribute("categoryList", categoryDAO.list());
+
+		session.setAttribute("supplierList", supplierDAO.list());
+
+		log.debug("Ending of the method onLoad");
 		return mv;
 	}
-	
-	@RequestMapping("/login")
-	public ModelAndView showLoginPage(){
-		ModelAndView mv = new ModelAndView("home");
-		mv.addObject("msg","You clicked on login link");
-		mv.addObject("showLoginPage","true");
+
+	@RequestMapping("/registerHere")
+	public ModelAndView registerHere() {
+		log.debug("Starting of the method registerHere");
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("user", user);
+		mv.addObject("isUserClickedRegisterHere", "true");
+		log.debug("Ending of the method registerHere");
 		return mv;
 	}
-	
-	@RequestMapping("/register")
-	public ModelAndView showRegistrationPage(){
-		ModelAndView mv = new ModelAndView("home");
-		mv.addObject("msg","You clicked on registration link");
-		mv.addObject("showRegistrationPage","true");
+
+	@RequestMapping("/loginHere")
+	public ModelAndView loginHere() {
+		log.debug("Starting of the method loginHere");
+		ModelAndView mv = new ModelAndView("/home");
+		mv.addObject("user", user);
+		mv.addObject("isUserClickedLoginHere", "true");
+		log.debug("Ending of the method loginHere");
 		return mv;
 	}
+
 	
-	@RequestMapping("/validate")
-	public ModelAndView validate(@RequestParam("email") String mail,@RequestParam("password") String pwd){
-		System.out.println("In validate method");
-		System.out.println("email:"+mail);
-		System.out.println("password"+pwd);
-		ModelAndView mv = new ModelAndView("home");
-		UserDAOImpl userDao = new UserDAOImpl();
-		if(userDao.isValidCredentials(mail, pwd)==true){
-			mv.addObject("successMsg","You are logged in successfully");
-		}
-		else{
-			mv.addObject("errorMsg","Invalid Credentials,Please try again");
-		}
-		return mv;
-	}
-	
+
 	@RequestMapping("/cart")
-	public ModelAndView showCartPage(){
+	public ModelAndView showCartPage() {
 		ModelAndView mv = new ModelAndView("home");
-		mv.addObject("msg","Your cart is Empty");
-		mv.addObject("showCartPage","true");
+		mv.addObject("msg", "Your cart is Empty");
+		mv.addObject("showCartPage", "true");
 		return mv;
 	}
+
 	
-	@RequestMapping("/buy")
-	public ModelAndView showBuyPage(){
-		ModelAndView mv = new ModelAndView("home");
-		mv.addObject("msg","Sorry you need to sign up to buy a product");
-		mv.addObject("showBuyPage","true");
-		return mv;
-	}
-	
-	
-	
-	
-	
-	
+
 }
---
+++ UserController.java
@@ -0,0 +1,119 @@
+package com.niit.shopgirl.Controller;
+
+
+import javax.servlet.http.HttpSession;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.shopgirlbackend.dao.CategoryDAO;
+
+import com.niit.shopgirlbackend.dao.SupplierDAO;
+import com.niit.shopgirlbackend.dao.UserDAO;
+import com.niit.shopgirlbackend.model.Category;
+import com.niit.shopgirlbackend.model.Product;
+import com.niit.shopgirlbackend.model.Supplier;
+import com.niit.shopgirlbackend.model.User;
+
+@Controller
+public class UserController{
+		
+		public static Logger log = LoggerFactory.getLogger(UserController.class);
+		
+		@Autowired
+		private UserDAO userDAO;
+		
+		@Autowired
+		private User user;
+		
+		@Autowired
+		private CategoryDAO categoryDAO;
+		
+		@Autowired
+		private Category category;
+		
+		@Autowired
+		private SupplierDAO supplierDAO;
+		
+		@Autowired
+		private Supplier supplier;
+		
+		@Autowired
+		private Product product;
+		
+		@Autowired
+		private HttpSession session;
+		
+		
+		
+		@RequestMapping(value="/validate", method = RequestMethod.POST)
+		public ModelAndView validateNow(@RequestParam(value="id")String userId,
+				@RequestParam(value="password")String password,HttpSession session){
+			
+			log.debug("Starting of the method validate");
+			ModelAndView mv = new ModelAndView("/home");
+			user = userDAO.isValidUser(userId, password);
+			System.out.println("i am in login method");
+			if(user != null){
+				log.debug("Valid Credentials");
+				session.setAttribute("loggedInUser", user.getName());
+				session.setAttribute("loggedInUserId", user.getId());
+				session.setAttribute("user", user);
+			
+			if(user.getRole().equals("ROLE_ADMIN")){
+				log.debug("logged in as Admin");
+				mv.addObject("isAdmin","true");
+				session.setAttribute("category",category);
+				session.setAttribute("categoryList", categoryDAO.list());
+				session.setAttribute("product", product);
+				
+				session.setAttribute("supplier",supplier);
+				session.setAttribute("supplierList", supplierDAO.list());
+			}
+			/*else{
+			log.debug("logged in as Enduser");
+			mv.addObject("isAdmin","false");
+			
+			}*/
+		}
+			else{
+				log.debug("Invalid credentials");
+				mv.addObject("invalidCredentials","true");
+				mv.addObject("errorMessage","Invalid Credentials");
+			}
+			log.debug("End of the method: validateNow");
+			return mv;
+			
+					
+}
+		
+		@RequestMapping(value = "/register", method = RequestMethod.POST)
+		public ModelAndView registerUser(@ModelAttribute User user) {
+			log.debug("Starting of the method registerUser");
+			ModelAndView mv = new ModelAndView("/home");
+			if (userDAO.get(user.getId()) == null) {
+				user.setRole("ROLE_USER"); // all the users are end users by default
+				userDAO.saveOrUpdate(user);
+				log.debug("You are successfully register");
+				mv.addObject("successMessage", "You are successfully registered");
+			} else {
+				log.debug("User exist with this id");
+				mv.addObject("errorMessage", "User exist with this id");
+			}
+			log.debug("Ending of the method registerUser");
+			return mv;
+		}
+
+
+	}
+
+
